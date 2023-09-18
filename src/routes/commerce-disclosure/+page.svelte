50eae3de8db18070567d14e895629dad566b5ec6
Tuan Chau <tuanchau.ict@gmail.com>
2023-09-18 17:54:04 +0900

    Remove unnecessary information

--- src/routes/commerce-disclosure/+page.svelte
+++ src/routes/commerce-disclosure/+page.svelte
@@ -1,14 +1,6 @@
 <script>
     import HtmlHead from "../../components/HtmlHead.svelte";
     import CommerceDisclosure from "../../components/commerce-disclosure/CommerceDisclosure.svelte";
-
-    let head = {
-        title: 'MonoSketch',
-        description: 'The official guide to SvelteKit, a framework for building web applications of all sizes, with a beautiful development experience and flexible filesystem-based routing.',
-        keywords: 'svelte, sveltekit, sapper, svelte kit, sveltekit tutorial, sveltekit guide, sveltekit docs, sveltekit documentation, sveltekit examples, sveltekit getting started, sveltekit getting started guide, sveltekit getting started tutorial, sveltekit getting started docs, sveltekit getting started documentation, sveltekit getting started examples, sveltekit getting started guide, sveltekit getting started tutorial, sveltekit getting started docs, sveltekit getting started documentation, sveltekit getting started examples, sveltekit getting started guide, sveltekit getting started tutorial, sveltekit getting started docs, sveltekit getting started documentation, sveltekit getting started examples, sveltekit getting started guide, sveltekit getting started tutorial, sveltekit getting started docs, sveltekit getting started documentation, sveltekit getting started examples',
-        image: 'https://kit.svelte.dev/images/preview.png',
-        url: 'https://kit.svelte.dev'
-    }
 </script>
 
 <HtmlHead title="MonoSketch - Unleash your ideas with ASCII" />
